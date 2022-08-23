9a7429b275a55e31ab88ee8cb8ef9496e6df0265
Mina Hanna Fayez <81904314+1mina1@users.noreply.github.com>
2022-08-23 10:18:33 +0200

    Add files via upload

---
+++ Backend/dft/dft_script.tcl
@@ -0,0 +1,194 @@
+
+########################### Define Top Module ############################
+                                                   
+set top_module SYSTEM_TOP_dft
+
+##################### Define Working Library Directory ######################
+                                                   
+define_design_lib work -path ./work
+
+############################# Formality Setup File ##########################
+                                                   
+set_svf $top_module.svf
+
+################## Design Compiler Library Files #setup ######################
+
+puts "###########################################"
+puts "#      #setting Design Libraries          #"
+puts "###########################################"
+
+#Add the path of the libraries and RTL files to the search_path variable
+lappend search_path /home/IC/tsmc_fb_cl013g_sc/aci/sc-m/synopsys
+lappend search_path /home/IC/Projects/System/RTL/ALU
+lappend search_path /home/IC/Projects/System/RTL/Clock_Divider
+lappend search_path /home/IC/Projects/System/RTL/Clock_Gating
+lappend search_path /home/IC/Projects/System/RTL/DATA_SYNC
+lappend search_path /home/IC/Projects/System/RTL/BIT_SYNC
+lappend search_path /home/IC/Projects/System/RTL/RegFile
+lappend search_path /home/IC/Projects/System/RTL/RST_SYNC
+lappend search_path /home/IC/Projects/System/RTL/SYS_CTRL
+lappend search_path /home/IC/Projects/System/RTL/UART/UART_RX
+lappend search_path /home/IC/Projects/System/RTL/UART/UART_TX
+lappend search_path /home/IC/Projects/System/RTL/UART/UART_TOP
+lappend search_path /home/IC/Projects/System/RTL/Top
+
+
+set SSLIB "scmetro_tsmc_cl013g_rvt_ss_1p08v_125c.db"
+set TTLIB "scmetro_tsmc_cl013g_rvt_tt_1p2v_25c.db"
+set FFLIB "scmetro_tsmc_cl013g_rvt_ff_1p32v_m40c.db"
+
+## Standard Cell libraries 
+set target_library [list $SSLIB $TTLIB $FFLIB]
+
+## Standard Cell & Hard Macros libraries 
+set link_library [list * $SSLIB $TTLIB $FFLIB]  
+
+######################## Reading RTL Files #################################
+
+puts "###########################################"
+puts "#             Reading RTL Files           #"
+puts "###########################################"
+
+set file_format verilog
+
+read_file -format $file_format ALU.v
+read_file -format $file_format ClkDiv.v
+read_file -format $file_format Clock_Gating.v
+read_file -format $file_format BIT_SYNC.v
+read_file -format $file_format DATA_SYNC.v
+read_file -format $file_format Register_FIle.v
+read_file -format $file_format RST_SYNC.v
+read_file -format $file_format RX_Controller.v
+read_file -format $file_format TX_Controller.v
+read_file -format $file_format SYS_Controller.v
+read_file -format $file_format Data_Sampler.v
+read_file -format $file_format Deseralizer.v
+read_file -format $file_format edge_bit_counter.v
+read_file -format $file_format Parity_Check.v
+read_file -format $file_format Stop_Check.v
+read_file -format $file_format Strt_Check.v
+read_file -format $file_format UART_RX.v
+read_file -format $file_format RXFSM.v
+read_file -format $file_format MUX.v
+read_file -format $file_format Parity_Calc.v
+read_file -format $file_format InputBuffer.v
+read_file -format $file_format counter.v
+read_file -format $file_format Parallel_To_Serial.v
+read_file -format $file_format Seralizer.v
+read_file -format $file_format Uart_TX_Top.v
+read_file -format $file_format FSM.v
+read_file -format $file_format UART.v
+read_file -format $file_format SYSTEM_TOP_dft.v
+read_file -format $file_format mux2X1.v
+###################### Defining toplevel ###################################
+
+current_design $top_module
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## Liniking All The Design Parts ########"
+puts "###############################################"
+
+link 
+
+#################### Liniking All The Design Parts #########################
+puts "###############################################"
+puts "######## checking design consistency ##########"
+puts "###############################################"
+
+check_design
+
+#################### Define Design Constraints #########################
+puts "###############################################"
+puts "############ Design Constraints #### ##########"
+puts "###############################################"
+
+source ./cons.tcl
+
+#################### Archirecture Scan Chains #########################
+puts "###############################################"
+puts "############ Configure scan chains ############"
+puts "###############################################"
+set_scan_configuration -clock_mixing no_mix  \
+                       -style multiplexed_flip_flop \
+                       -replace true -max_length 100  
+
+
+###################### Mapping and optimization ########################
+puts "###############################################"
+puts "########## Mapping & Optimization #############"
+puts "###############################################"
+
+
+compile -scan
+
+################################################################### 
+# Setting Test Timing Variables
+################################################################### 
+
+# Preclock Measure Protocol (default protocol)
+set test_default_period 100
+set test_default_delay 0
+set test_default_bidir_delay 0
+set test_default_strobe 20
+set test_default_strobe_width 0
+
+########################## Define DFT Signals ##########################
+
+set_dft_signal -port [get_ports scan_clk]  -type ScanClock   -view existing_dft  -timing {30 60}
+set_dft_signal -port [get_ports scan_rst]  -type Reset       -view existing_dft  -active_state 0
+set_dft_signal -port [get_ports test_mode] -type Constant    -view existing_dft  -active_state 1 
+set_dft_signal -port [get_ports test_mode] -type TestMode    -view spec          -active_state 1 
+set_dft_signal -port [get_ports SE]        -type ScanEnable  -view spec          -active_state 1   -usage scan
+set_dft_signal -port [get_ports SI]        -type ScanDataIn  -view spec 
+set_dft_signal -port [get_ports SO]        -type ScanDataOut -view spec  
+
+############################# Create Test Protocol #####################
+
+create_test_protocol
+                            
+###################### Pre-DFT Design Rule Checking ####################
+
+dft_drc -verbose
+
+############################# Preview DFT ##############################
+
+preview_dft -show scan_summary
+
+############################# Insert DFT ###############################
+
+insert_dft
+compile -scan -incremental
+
+######################## Optimize Logic post DFT #######################
+
+compile -scan -incremental
+
+###################### Design Rule Checking post DFT ###################
+
+dft_drc -verbose -coverage_estimate
+
+#############################################################################
+# Write out files
+#############################################################################
+
+write_file -format verilog -hierarchy -output netlists/$top_module.ddc
+write_file -format verilog -hierarchy -output netlists/$top_module.v
+write_sdf  sdf/$top_module.sdf
+write_sdc  -nosplit sdc/$top_module.sdc
+
+####################### reporting ##########################################
+
+report_area -hierarchy > reports/area.rpt
+report_power -hierarchy > reports/power.rpt
+report_timing -delay_type min -max_paths 20 > reports/hold.rpt
+report_timing -delay_type max -max_paths 20 > reports/setup.rpt
+report_clock -attributes > reports/clocks.rpt
+report_constraint -all_violators -nosplit > reports/constraints.rpt
+
+set_svf -off
+################# starting graphical user interface #######################
+
+#gui_start
+
+#exit
